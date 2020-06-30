13272e94aacc28228aef696bea97dd91cb4f5b8d
Sunil Kumar <45172271+edocbuhtig@users.noreply.github.com>
2020-07-01 00:41:11 +0530

    Update to Bootstrap 5
    Removed jquery

--- functions.php
+++ functions.php
@@ -2,16 +2,16 @@
 
 function enqueue_styles() {
 
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/libs/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' );
     wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles');
 function enqueue_scripts() {
 
-    wp_enqueue_script( 'jqslim', get_template_directory_uri() . '/libs/jquery-3.4.1.slim.min.js' );
+    //wp_enqueue_script( 'jqslim', get_template_directory_uri() . '/libs/jquery-3.4.1.slim.min.js' );
     wp_enqueue_script( 'popper', get_template_directory_uri() . '/libs/popper.min.js' );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/libs/bootstrap.min.js' );
+    wp_enqueue_script( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
 
